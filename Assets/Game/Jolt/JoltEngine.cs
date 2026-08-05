6af3d390fb88838685911fd1044f71bd13c7574c
Greenie709 <greenie709@gmail.com>
2026-08-05 19:15:42 +0530

    Jolt Ball and box test

--- Assets/Game/Jolt/JoltEngine.cs
+++ Assets/Game/Jolt/JoltEngine.cs
@@ -1,4 +1,5 @@
 using System;
+using Game.Jolt;
 using Unity.Scripting.LifecycleManagement;
 using UnityEngine;
 
@@ -15,6 +16,11 @@ public class JoltEngine : MonoBehaviour
         _instance = jeo.AddComponent<JoltEngine>();
     }
 
+    private JoltWorld _activeWorld;
+
+    private JoltBodyHandle _boxHandle;
+    private JoltBodyHandle _ballHandle;
+
     private void Awake()
     {
         if (_instance && _instance != this)
@@ -23,17 +29,47 @@ public class JoltEngine : MonoBehaviour
             return;
         }
 
-        RunTest();
+        InitJoltWorld();
     }
 
     private void OnDestroy()
     {
+        DisposeJoltWorld();
+        
         if (_instance == this) _instance = null;
     }
 
 
-    private void RunTest()
+    private void InitJoltWorld()
     {
-        Debug.Log("Testing JoltEngine");
+        _activeWorld = new JoltWorld(1024);
+        
+        // Spawn a box
+        _boxHandle = _activeWorld.AddBody(JoltBodyDesc.Box(Vector3.one * 0.5f, Vector3.zero, Quaternion.identity,
+            JoltMotion.Static));
+        
+        // Spawn a ball
+        _ballHandle = _activeWorld.AddBody(JoltBodyDesc.Sphere(0.3f, new Vector3(0, 2, 0), JoltMotion.Dynamic));
+    }
+
+    private void DisposeJoltWorld()
+    {
+        _activeWorld?.Dispose();
+        _activeWorld = null;
+    }
+
+    private void FixedUpdate()
+    {
+        // Read ball and box
+        
+        if (_activeWorld == null) return;
+        
+        _activeWorld.Step(Time.fixedDeltaTime);
+        
+        var states = _activeWorld.ReadStates();
+        foreach (JoltBodyState s in states)
+        {
+            Debug.Log(s.Position);
+        }
     }
 }
